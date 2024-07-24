15bf0fcd7e1d649b7efecb2c95874ecb7769fd80
Robbie <robbie331@>
2024-07-24 15:21:48 +0900

    양의 정수 2개(0 포함)와 연산 기호를 매개변수로 받아 사칙연산(+,-,*,/) 기능을 수행한 후 결과 값을 반환하는 메서드와 연산 결과를 저장하는 컬렉션 타입 필드를 가진 Calculator 클래스를 생성합니다.

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,5 +1,28 @@
 package calculator;
 
-public class Calculator {
+import java.util.LinkedList;
+import java.util.Queue;
 
+public class Calculator {
+    private Queue<Double> resultQueue;
+
+    public Calculator() {
+        this.resultQueue = new LinkedList<>();
+    }
+
+    public double calculate(int num1, int num2, char operator) {
+        double result = switch (operator) {
+            case '+' -> num1 + num2;
+            case '-' -> num1 - num2;
+            case '*' -> num1 * num2;
+            case '/' -> {
+                if (num2 == 0) throw new IllegalArgumentException("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+                yield (double) num1 / num2;
+            }
+            default -> throw new IllegalArgumentException("잘못된 사칙연산 기호 입력입니다.");
+        };
+
+        resultQueue.add(result);
+        return result;
+    }
 }
