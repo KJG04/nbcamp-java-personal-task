0b3f06196ff8f049120afdd5f5bca1a56fb38cd6
Robbie <robbie331@>
2024-07-24 11:24:16 +0900

    반복문을 사용하여 반복의 종료를 알려주는 “exit” 문자열을 입력하기 전까지 무한으로 계산을 진행할 수 있도록 소스 코드를 수정합니다.

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -7,42 +7,49 @@ public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("첫 번째 숫자를 입력하세요: ");
-        int num1 = sc.nextInt();
-        sc.nextLine(); //개행문자 제거
+        while (true) {
+            System.out.print("첫 번째 숫자를 입력하세요: ");
+            int num1 = sc.nextInt();
+            sc.nextLine(); //개행문자 제거
 
-        System.out.print("두 번째 숫자를 입력하세요: ");
-        int num2 = sc.nextInt();
-        sc.nextLine(); //개행문자 제거
+            System.out.print("두 번째 숫자를 입력하세요: ");
+            int num2 = sc.nextInt();
+            sc.nextLine(); //개행문자 제거
 
-        try {
-            System.out.print("사칙연산 기호를 입력하세요: ");
-            char operator = sc.nextLine().charAt(0);
-            int result = 0;
-            switch (operator) {
-                case '+':
-                    result = num1 + num2;
-                    break;
-                case '-':
-                    result = num1 - num2;
-                    break;
-                case '*':
-                    result = num1 * num2;
-                    break;
-                case '/':
-                    result = num1 / num2;
-                    break;
-                default:
-                    System.out.println("잘못된 사칙연산 기호 입력입니다.");
-                    return;
+            try {
+                System.out.print("사칙연산 기호를 입력하세요: ");
+                char operator = sc.nextLine().charAt(0);
+                int result = 0;
+                switch (operator) {
+                    case '+':
+                        result = num1 + num2;
+                        break;
+                    case '-':
+                        result = num1 - num2;
+                        break;
+                    case '*':
+                        result = num1 * num2;
+                        break;
+                    case '/':
+                        result = num1 / num2;
+                        break;
+                    default:
+                        System.out.println("잘못된 사칙연산 기호 입력입니다.");
+                        continue;
+                }
+                System.out.println("결과: " + result);
+
+            } catch (StringIndexOutOfBoundsException e) {
+                // sc.nextLine().charAt(0) 에서 오류가 발생했을때
+                System.out.println("잘못된 사칙연산 기호 입력입니다.");
+            } catch (ArithmeticException e) {
+                System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
             }
-            System.out.println("결과: " + result);
 
-        } catch (StringIndexOutOfBoundsException e) {
-            // sc.nextLine().charAt(0) 에서 오류가 발생했을때
-            System.out.println("잘못된 사칙연산 기호 입력입니다.");
-        } catch (ArithmeticException e) {
-            System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+            String terminationIntent = sc.nextLine();
+            if (terminationIntent.equals("exit"))
+                break;
         }
     }
 }
