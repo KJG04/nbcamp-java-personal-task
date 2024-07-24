2b5ad214134a0f10dc380d1a9004b2d7cf3addaa
Robbie <robbie331@>
2024-07-24 11:48:07 +0900

    연산 결과 10개를 저장할 수 있는 배열을 선언 및 생성하고 연산의 결과를 저장합니다.

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -1,20 +1,33 @@
 package calculator;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class App {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int[] resultList = new int[10];
+        int resultListIndex = 0;
 
         while (true) {
-            System.out.print("첫 번째 숫자를 입력하세요: ");
-            int num1 = sc.nextInt();
-            sc.nextLine(); //개행문자 제거
+            int num1;
+            int num2;
 
-            System.out.print("두 번째 숫자를 입력하세요: ");
-            int num2 = sc.nextInt();
-            sc.nextLine(); //개행문자 제거
+            try {
+                System.out.print("첫 번째 숫자를 입력하세요: ");
+                num1 = sc.nextInt();
+                sc.nextLine(); //개행문자 제거
+
+                System.out.print("두 번째 숫자를 입력하세요: ");
+                num2 = sc.nextInt();
+                sc.nextLine(); //개행문자 제거
+            } catch (InputMismatchException e) {
+                System.out.println("숫자를 입력하세요.");
+                sc.nextLine();
+                continue;
+            }
+
 
             try {
                 System.out.print("사칙연산 기호를 입력하세요: ");
@@ -38,15 +51,21 @@ public class App {
                         continue;
                 }
                 System.out.println("결과: " + result);
-
+                // 결과 배열에 저장
+                resultList[resultListIndex] = result;
+                // 다음에 저장할 위치를 바꾼다
+                // 최대로 resultList.length - 1 이 되게끔 함
+                resultListIndex = Math.min(resultList.length - 1, resultListIndex + 1);
             } catch (StringIndexOutOfBoundsException e) {
                 // sc.nextLine().charAt(0) 에서 오류가 발생했을때
                 System.out.println("잘못된 사칙연산 기호 입력입니다.");
+                continue;
             } catch (ArithmeticException e) {
                 System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+                continue;
             }
 
-            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+            System.out.print("더 계산하시겠습니까? (exit 입력 시 종료): ");
             String terminationIntent = sc.nextLine();
             if (terminationIntent.equals("exit"))
                 break;
