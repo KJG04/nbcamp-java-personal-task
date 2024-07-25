68839efca7e658ccb95b87d4440e092b7ab7d79e
Robbie <robbie331@>
2024-07-25 13:25:34 +0900

    ArithmeticCalculator 클래스의 연산 메서드에 책임(역할)이 많아 보입니다. 사칙연산 각각의 기능을 담당하는 AddOperator, SubtractOperator, MultiplyOperator, DivideOperator 클래스를 만들어 연산 메서드의 책임을 분리 해봅니다. (SRP)

--- src/main/java/calculator/ArithmeticCalculator.java
+++ src/main/java/calculator/ArithmeticCalculator.java
@@ -1,15 +1,25 @@
 package calculator;
 
 public class ArithmeticCalculator extends Calculator {
+    private AddOperator addOperator;
+    private SubtractOperator subtractOperator;
+    private MultiplyOperator multiplyOperator;
+    private DivideOperator divideOperator;
+
+    public ArithmeticCalculator() {
+        super();
+        this.addOperator = new AddOperator();
+        this.subtractOperator = new SubtractOperator();
+        this.multiplyOperator = new MultiplyOperator();
+        this.divideOperator = new DivideOperator();
+    }
+
     public double calculate(int num1, int num2, char operator) {
         return switch (operator) {
-            case '+' -> num1 + num2;
-            case '-' -> num1 - num2;
-            case '*' -> num1 * num2;
-            case '/' -> {
-                if (num2 == 0) throw new IllegalArgumentException("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
-                yield (double) num1 / num2;
-            }
+            case '+' -> this.addOperator.operate(num1, num2);
+            case '-' -> this.subtractOperator.operate(num1, num2);
+            case '*' -> this.multiplyOperator.operate(num1, num2);
+            case '/' -> this.divideOperator.operate(num1, num2);
             default -> throw new IllegalArgumentException("잘못된 사칙연산 기호 입력입니다.");
         };
     }
